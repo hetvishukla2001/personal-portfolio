d62c333aa77320987991e6592f1fe74b922df0b4
hetvishukla2001 <hetvishukla2001@gmail.com>
2024-08-26 16:26:46 -0400

    final

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import { Footer } from './Components/Footer';
-
+ 
 function App() {
   return (
     <div className="App">
